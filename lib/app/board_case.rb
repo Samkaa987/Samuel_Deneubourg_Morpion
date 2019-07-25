82c21459717c1b95efebd53708ee220ccea31a2e
Samkaa987 <deneubourg.samuel@gmail.com>
2019-07-25 17:40:06 +0200

    commit ca passe creme mais on peut faire mieux mais en vrai c'est déjà bien

---
+++ lib/app/board_case.rb
@@ -0,0 +1,28 @@
+class BoardCase
+	attr_accessor :value
+
+
+
+	def initialize
+
+		@value = " "
+		
+	end
+
+
+
+	def case_x
+
+		@value = "x"
+		
+	end
+
+
+
+	def case_o
+
+		@value = "o"
+		
+	end
+
+end
